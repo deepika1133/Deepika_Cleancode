3f4fc9d03d57b3bafe6abb5bd4eeb3cd571f57fa
deepika1133 <60563715+deepika1133@users.noreply.github.com>
2020-02-21 06:24:10 -0800

    Add files via upload

---
+++ EpamTask4/src/main/java/com/deepika/InvalidMaterialSelect.java
@@ -0,0 +1,16 @@
+package com.deepika;
+
+@SuppressWarnings("serial")
+public class InvalidMaterialSelect extends Exception  {
+
+
+
+    public InvalidMaterialSelect(String message) {
+
+        super(message);
+
+    }
+
+}
+
+
---
+++ EpamTask4/src/main/java/com/deepika/MaterialStandards.java
@@ -0,0 +1,27 @@
+package com.deepika;
+
+public class MaterialStandards {
+
+	public enum Standards {
+
+        standardMaterial, AboveStandardMaterial, HighStandardMaterial;
+
+    }
+
+
+
+    public static void getMaterialType() {
+
+        Standards[] standards = Standards.values();
+
+        for (Standards s : standards) {
+
+            System.out.println("Type '" + s + "' for " + s);
+
+        }
+
+    }
+
+
+
+}
